0d53cead733baaa06c5b9d08a1ce8bfc4998cd55
belre <neopossam@gmail.com>
2018-08-14 13:15:22 +0900

    Simple BBS Source Code

---
+++ sinatra/practice_simplebbs/app.rb
@@ -0,0 +1,47 @@
+
+
+Bundler.require
+
+
+require 'sinatra/reloader'
+require 'active_record'
+
+# Establish Connection to sqlite3
+ActiveRecord::Base.establish_connection(
+ adapter: 'sqlite3',
+ database: './bbs.db'
+)
+
+class Comment < ActiveRecord::Base
+ validates :body, presence: true
+end
+
+
+get '/' do
+ @title = "My BBS"
+ @comments = Comment.all
+ erb :index
+end
+
+post '/create' do
+ Comment.create(body: params[:body])
+ redirect to('/')
+end
+
+get '/rmcomment' do
+ if(params[:selcomment] != nil)
+  md = params[:selcomment].match(/chk(\d+)/)
+  if(md != nil) # com no is saved as md[1]
+   comm = Comment.find((md[1]).to_i(10))
+   comm.destroy
+  end 
+ end
+
+ redirect to('/')
+end
+
+get '/rmcommentall' do
+ Comment.destroy_all
+ redirect to('/')
+end
+
