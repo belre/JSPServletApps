8ccbfa54921a2b4702ca2ddc3adc6e0f0eeba2b8
belre <neopossam@gmail.com>
2018-08-15 13:12:07 +0900

    Working compare

--- sinatra/practice_simplebbs/app.rb
+++ sinatra/practice_simplebbs/app.rb
@@ -5,6 +5,11 @@ Bundler.require
 
 require 'sinatra/reloader'
 require 'active_record'
+require 'rack/csrf'
+
+
+use Rack::Session::Cookie, secret: "thisissomethingsecret"
+use Rack::Csrf, raise: true
 
 # Establish Connection to sqlite3
 ActiveRecord::Base.establish_connection(
@@ -28,17 +33,22 @@ post '/create' do
  redirect to('/')
 end
 
-get '/rmcomment' do
- if(params[:selcomment] != nil)
-  md = params[:selcomment].match(/chk(\d+)/)
-  if(md != nil) # com no is saved as md[1]
-   comm = Comment.find((md[1]).to_i(10))
-   comm.destroy
-  end 
- end
-
- redirect to('/')
+post '/destroy' do  
+ puts params[:_csrf]
+ Comment.find(params[:id]).destroy
 end
+
+#get '/rmcomment' do
+# if(params[:selcomment] != nil)
+#  md = params[:selcomment].match(/chk(\d+)/)
+#  if(md != nil) # com no is saved as md[1]
+#   comm = Comment.find((md[1]).to_i(10))
+#   comm.destroy
+#  end 
+# end
+
+# redirect to('/')
+#end
 
 get '/rmcommentall' do
  Comment.destroy_all
---
+++ sinatra/practice_simplebbs/public/js/main.js
@@ -0,0 +1,18 @@
+
+$(function() {
+ $('.delete').on('click', function() {
+  var li = $(this).parent();
+  if(!confirm('sure?')) {
+   return;
+  }
+  $.post('/destroy', {
+   id: li.data('id'),
+   _csrf: li.data('token')
+  }, function () {
+   li.fadeOut(800);
+  });
+ });
+});
+
+
+
